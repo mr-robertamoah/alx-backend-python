ca7d861f75d285b101f7ca54b538d66d86e99255
Robert Amoah <mr_robertamoah@yahoo.com>
2024-08-03 04:09:15 +0000

    done with 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    annotations of a function and its parameters.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Returns a float which is a sum of two floats.
+    '''
+    return a + b
