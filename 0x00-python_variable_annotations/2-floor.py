5fa0e357709a3d9faa2725d86aa2023ee9cf0011
Robert Amoah <mr_robertamoah@yahoo.com>
2024-08-03 04:28:38 +0000

    done with 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+contains annotated floor function
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float
+    """
+    return math.floor(n)
