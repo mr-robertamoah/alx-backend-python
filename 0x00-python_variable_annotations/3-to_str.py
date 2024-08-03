fc69c3ef53d03551518f01853c6adb29df0eb41c
Robert Amoah <mr_robertamoah@yahoo.com>
2024-08-03 04:31:19 +0000

    done with 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+contains annotated to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float
+    """
+    return str(n)
