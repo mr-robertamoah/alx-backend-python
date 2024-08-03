1ae9bea45b809771c81940949696adb36d6cc7ec
Robert Amoah <mr_robertamoah@yahoo.com>
2024-08-03 04:25:24 +0000

    done with 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+contains annotated concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns concatenation of two strings
+    """
+    return str1 + str2
